1c1a9749cd02341bb4c762d6f00a54cb509c7cb5
Pedro Gregori <ferreiragregorimelo@gmail.com>
2025-05-13 19:32:54 -0300

    Sctructure

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, request, render_template, redirect, url_for 
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    return render_template('index.html')
