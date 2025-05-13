eba549df44b03df6234d7a767b196cb13a073501
Pedro Gregori <ferreiragregorimelo@gmail.com>
2025-05-13 19:41:37 -0300

    Flask app running

--- app.py
+++ app.py
@@ -7,3 +7,6 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route('/')
 def index():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
